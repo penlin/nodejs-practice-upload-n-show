75e09c9fdebcd96da21046a1b7edca850a737f18
LinKuan-yu <penlin@Penlin.local>
2015-05-23 02:41:40 +0800

    [M] export server.js as a module and execute through index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+/*
+ This is the main executing .js file
+*/
+
+var server = require("./server");
+
+server.start();
--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 /*
-This is a test node.js server program that would only response "Hello Node!" 
+This is a test node.js module that would start a simple echo server which only response "Hello Node!" 
 */
 
 var http = require("http");
 var PORT = 8888;
 
-function onRequest (request, response) {
-	console.log("Request from " + request.connection.remoteAddress);
-    response.writeHead(200, {"Content-Type": "text/plain"});       
-    response.write("Hello Node!");
-    response.end();
+function start(){
+	function onRequest (request, response) {
+		console.log("Request from " + request.connection.remoteAddress);
+	    response.writeHead(200, {"Content-Type": "text/plain"});       
+	    response.write("Hello Node!");
+	    response.end();
+	};
+
+	http.createServer(onRequest).listen(PORT);
+
+	console.log("Server starts @ port:"+PORT);
 };
 
-http.createServer(onRequest).listen(PORT);
-
-console.log("Server starts @ port:"+PORT);
+exports.start=start;
